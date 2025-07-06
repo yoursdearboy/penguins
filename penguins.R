d12619fafd850b08e137c9b36fa1f3a71b95df21
Kirill Voronin <yoursdearboy@gmail.com>
2025-07-06 14:32:44 +0300

    Сохранили гистограмму длины клюва для публикации

--- penguins.R
+++ penguins.R
@@ -1,6 +1,6 @@
 data <- read.csv("penguins.csv")
 x <- data$bill_length_mm
 
-hist(x, breaks = seq(40, 60, 4))
+png("penguins-hist.png")
 hist(x, breaks = seq(40, 60, 2))
-hist(x, breaks = seq(40, 60, 1))
+dev.off()
