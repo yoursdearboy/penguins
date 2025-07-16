a5871728eb012433574faee8b41d91e8089934bb
Kirill Voronin <yoursdearboy@gmail.com>
2025-07-16 14:11:31 +0300

    Заменили гистограмму на ящик с усами.

--- penguins.R
+++ penguins.R
@@ -1,6 +1,6 @@
 data <- read.csv("penguins.csv")
 x <- data$bill_length_mm
 
-png("penguins-hist.png")
-hist(x, breaks = seq(40, 60, 2))
+png("penguins-boxplot.png")
+boxplot(x, ylab = "Bill length, mm")
 dev.off()
