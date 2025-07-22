193c2c688eaf15eb0989b2260d464c5a3ecd0267
Kirill Voronin <yoursdearboy@gmail.com>
2025-07-23 01:27:23 +0300

    Добавили подпись оси

--- penguins.R
+++ penguins.R
@@ -2,5 +2,5 @@ data <- read.csv("penguins.csv")
 x <- data$bill_length_mm
 
 png("penguins-hist.png")
-hist(x, breaks = seq(40, 60, 2))
+hist(x, breaks = seq(40, 60, 2), xlab = "Bill length, mm")
 dev.off()
