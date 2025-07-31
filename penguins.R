4c68a07ed9af72e95f251f307204096fffff46ec
Kirill Voronin <yoursdearboy@gmail.com>
2025-07-31 17:06:34 +0300

    Добавлен комментарий к скрипту

--- penguins.R
+++ penguins.R
@@ -2,6 +2,8 @@ data <- read.csv("penguins.csv")
 x <- data$bill_length_mm
 g <- data$sex
 
+# Ящик с усами для длины клюва
+
 png("penguins-boxplot.png")
 boxplot(x ~ g, xlab = "Sex", ylab = "Bill length, mm")
 dev.off()
